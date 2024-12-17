afa63d447421c3e3dab3f1189ffb9d69a3840049
DEEPASRI-MAHENDRABABU <79656820+DEEPASRI-MAHENDRABABU@users.noreply.github.com>
2024-12-17 15:38:41 +0530

    Update readFileModule.js

--- Filesystem(fs)/readFileModule.js
+++ Filesystem(fs)/readFileModule.js
@@ -2,7 +2,7 @@ const fs=require('fs');
 
 const path=require('path');
 
-fs.readFile("./files/start.txt",function(err,data){ // WITHOUT USING PATH
+fs.readFile("./files/start.txt",function(err,data){ // WITHOUT USING PATH.JOIN
    if(err){
     console.log("Error");
    }
@@ -18,7 +18,7 @@ fs.readFile(path.join(__dirname,'files','start.txt'), (err,data)=>{
     console.log(data.toString() );
     
 })
-//we can use both way for read and write file , the diff b/w this are by using path.join is for if you are using mac and windows we could confuss / \ so we use the path.join method
+
 
 
 
